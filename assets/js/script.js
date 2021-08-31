6dab7ae8a25a551a0c4b483d01c2830b2478eb1f
Jack Jac <k.a.jack21@gmail.com>
2021-08-31 14:26:07 -0400

    add queryselector and show element under event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
-var randomDrink = "https://www.thecocktaildb.com/api/json/v1/1/random.php" 
-var randomMeal = "www.themealdb.com/api/json/v1/1/random.php"
+var randomDrink = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+var randomMeal = "www.themealdb.com/api/json/v1/1/random.php";
 var selectElement = document.querySelector('select');
+var showEl = document.querySelector(".showEl");
+
 
 selectElement.addEventListener('change', (event) => {
+    showEl.classList.remove("hide");
     console.log(event.target.value);
   if (event.target.value === 1) {
     fetch (randomMeal)
