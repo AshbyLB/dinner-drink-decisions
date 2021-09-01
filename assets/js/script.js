289d49f859639a56007347d1e7192735c72abcbd
Ankit Srivastava <thisisankitsrivastava@gmail.com>
2021-09-01 07:05:19 +0530

    Card Resized and centered added sample image, Indentation Fixes
    Signed-off-by: Ankit Srivastava <thisisankitsrivastava@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,23 @@ var showEl = document.querySelector(".showEl");
 selectElement.addEventListener('change', (event) => {
     showEl.classList.remove("hide");
     console.log(event.target.value);
-  if (event.target.value === 1) {
-    fetch (randomMeal)
-    .then(function(response){
-        return response.JSON();
-    })
-    .then (function(data){
+    if (event.target.value === 1) {
+        fetch(randomMeal)
+            .then(function (response) {
+                return response.JSON();
+            })
+            .then(function (data) {
 
-    })
+            })
 
-    } else{
-        fetch (randomDrink)
-    .then(function(response){
-        return response.JSON();
-    })
-    .then (function(data){
+    } else {
+        fetch(randomDrink)
+            .then(function (response) {
+                return response.JSON();
+            })
+            .then(function (data) {
 
-    })
+            })
     }
-  
+
 });
